8261d2aaee83d8edbe3c7fec03a2a31b761fee38
lucasmt01 <lucasedu.mt01@gmail.com>
2026-01-02 09:28:12 -0300

    ajuste na autenticação de role para o fluxo de convênio e pagamento

--- ValidationInterface/src/main/java/sdtrabalho/validation/api/security/SecurityConfig.java
+++ ValidationInterface/src/main/java/sdtrabalho/validation/api/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package sdtrabalho.validation.api.security;
 
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -9,16 +11,17 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.*;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
@@ -53,7 +56,11 @@ public class SecurityConfig {
   Converter<Jwt, AbstractAuthenticationToken> jwtAuthConverter() {
     return jwt -> {
       var authorities = extractAuthorities(jwt);
-      return new JwtAuthenticationToken(jwt, authorities);
+      return new UsernamePasswordAuthenticationToken(
+          jwt.getSubject(),
+          null,
+          authorities
+      );
     };
   }
 
